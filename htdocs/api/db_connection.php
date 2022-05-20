467543aaf92f07c541251acd2340669bd1807605
Shanre Scheepers <21100387@virtualwindow.co.za>
2022-05-20 12:13:51 +0200

    ft imports ft ht

---
+++ htdocs/api/db_connection.php
@@ -0,0 +1,18 @@
+<?php
+
+$dbhost = "localhost";
+$dbusername = "root";
+$dbpassword = "root";
+$dbname = "pets";
+
+
+$conn = new mysqli($dbhost, $dbusername, $dbpassword , $dbname);
+
+    if($mysqli -> connect_errno){
+        echo 'Failed to connect to DB: ' . $mysqli -> connect_error;
+        exit();
+    }
+
+
+
+?>
